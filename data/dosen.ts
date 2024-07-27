30a6890101a92329c5ed2bbf4b9ca5556ea2aa0b
ab-azmi <azmi.kiva@gmail.com>
2024-07-27 07:00:29 +0700

    make list dosen page

---
+++ data/dosen.ts
@@ -0,0 +1,108 @@
+export const dosenTI = [
+    {
+        name: "Dr. Alamsyah S.Si., M.Kom.",
+        image: "https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/132320168",
+        email: "alamsyah@mail.unnes.ac.id",
+        badges: ["Ketua Jurusan", "TI", "+62 815-6513-617", "Mengajar", "NIP-197405172006041001"],
+    },
+    {
+        name: "Endang Sugiharti S.Si., M.Kom.",
+        image: "https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/132231407",
+        email: "endangsugiharti@mail.unnes.ac.id",
+        badges: ["TI", "+62 856-7833-877", "Mengajar", "NIP-197401071999032001"],
+    },
+    {
+        name: "Florentina Yuni Arini S.Kom., M.Cs., Ph.D.",
+        image:"",
+        email: "floyuna@mail.unnes.ac.id",
+        badges: ["TI", "Mengajar", "NIP-197810252003122001"],
+    },
+    {
+        name: "Zaenal Abidin S.Si., M.Cs., Ph.D.",
+        image:"https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/132308201",
+        email: "z.abidin@mail.unnes.ac.id",
+        badges: ["TI", "Mengajar", "NIP-198205042005011001"],
+    },
+    {
+        name: "Riza Arifudin S.Pd., M.Cs.",
+        image:"https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/132308204",
+        email: "rizaarifudin@mail.unnes.ac.id",
+        badges: ["TI", "Mengajar", "NIP-198005252005011001", "+62 857-2707-6118"],
+    },
+    {
+        name: "Much Aziz Muslim S.Kom., M.Kom.",
+        image:"",
+        email: "a212muslim@mail.unnes.ac.id",
+        badges: ["TI", "Mengajar", "NIP-197404202008121001"],
+    },
+    {
+        name: "Aji Purwinarko S.Si., M.Cs.",
+        image:"https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/198509102015041001",
+        email: "aji.purwinarko@mail.unnes.ac.id",
+        badges: ["TI", "Mengajar", "NIP-198509102015041001"],
+    },
+    {
+        name: "Anggyi Trisnawan Putra S.Si., M.Si.",
+        image:"https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/198707062014041003",
+        email: "anggy.trisnawan@mail.unnes.ac.id",
+        badges: ["TI", "Mengajar", "NIP-198707062014041003"],
+    },
+    {
+        name: "Abas Setiawan S.Kom., M.Cs.",
+        image:"https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/199110302022031006",
+        email: "abas.setiawan@mail.unnes.ac.id",
+        badges: ["TI", "Mengajar", "NIP-199110302022031006", "+62 899-5550-219"],
+    },
+    {
+        name: "Jumanto S.Kom., M.Cs.",
+        image:"https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/199202072023211024",
+        email: "jumanto@mail.unnes.ac.id",
+        badges: ["TI", "Mengajar", "NIP-199202072023211024"],
+    },
+    {
+        name: "M. Faris Al Hakim S.Pd., M.Cs.",
+        image:"https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/199203272022031003",
+        email: "farishakim@mail.unnes.ac.id",
+        badges: ["TI", "Mengajar", "NIP-199203272022031003"],
+    },
+    
+]
+
+export const dosenSI = [
+    {
+        name: "Budi Prasetiyo S.Si., M.Kom.",
+        image: "https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/198805012014041001",
+        email: "bprasetiyo@mail.unnes.ac.id",
+        badges: ["SI", "Mengajar", "NIP-198805012014041001"],
+    },
+    {
+        name: "Subhan S.Pd., M.Pd., M.Kom.",
+        image: "https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/198909042019031014",
+        email: "subhan@mail.unnes.ac.id",
+        badges: ["SI", "Mengajar", "NIP-198909042019031014", "+62 856-4005-2575"],
+    },
+    {
+        name: "Yahya Nur Ifriza S.Pd., M.Kom.",
+        image: "https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/199001172022031008",
+        email: "yahyanurifriza@mail.unnes.ac.id",
+        badges: ["SI", "Mengajar", "NIP-199001172022031008", "+62 856-4057-2391"],
+    },
+    {
+        name: "Kholiq Budiman S.Pd., M.Kom.",
+        image: "https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/199209242019031013",
+        email: "kholiq.budiman@mail.unnes.ac.id",
+        badges: ["SI", "Mengajar", "NIP-199209242019031013", "+62 857-2833-4774"],
+    },
+    {
+        name: "Devi Ajeng Efrilianda S.Kom., M.Kom.",
+        image: "https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/199304152019032012",
+        email: "deviajeng@mail.unnes.ac.id",
+        badges: ["SI", "Mengajar", "NIP-199304152019032012"],
+    },
+    {
+        name: "Yusuf Wisnu Mandaya S.T., M.Kom.",
+        image: "https://simpeg.unnes.ac.id/index.php/simpeg_portofolio/load_photo/1993051020230812001",
+        email: "wisnumandaya@mail.unnes.ac.id",
+        badges: ["SI", "Mengajar", "NIP-1993051020230812001"],
+    },
+]
