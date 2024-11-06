c86f94fa9d9921b5747e07162b5fe89d332c6d80
ab-azmi <azmi.kiva@gmail.com>
2024-11-06 20:09:46 +0800

    fix bug on dark mode toggle

--- components/dark-mode-toggle.tsx
+++ components/dark-mode-toggle.tsx
@@ -1,42 +1,60 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Moon, PcCase, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
-
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import * as React from "react";
+import { Monitor, Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
 
 export function ModeToggle() {
-  const { setTheme, theme } = useTheme()
+  const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+     
+    } else if (theme === "dark") {
+      setTheme("system");
+      
+    } else {
+      setTheme("light");
+      
+    }
+  };
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <div className="animate-spin-slow">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          </div>
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={toggleTheme}
+      className="relative overflow-hidden"
+    >
+      
+        <Moon
+          className={`h-[1.2rem] w-[1.2rem] absolute inset-0 m-auto ${
+            theme === "dark" ? "" : "hidden"
+          }`}
+        />
+        <Sun
+          className={`h-[1.2rem] text-orange-500 absolute inset-0 m-auto ${
+            theme === "light" ? "" : "hidden"
+          }`}
+        />
+        <Monitor
+          className={`h-[1.2rem] absolute inset-0 m-auto ${
+            theme === "system" ? "" : "hidden"
+          }`}
+        />
+      
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
 }
