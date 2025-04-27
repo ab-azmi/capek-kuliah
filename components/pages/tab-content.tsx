cba33473f760f72b71d4910d3cf9cc1255acaa56
Faris Faikar <farisfaikar.r@gmail.com>
2025-04-28 01:20:59 +0700

    format: minor indent and whitespace fixes

--- components/pages/tab-content.tsx
+++ components/pages/tab-content.tsx
@@ -21,7 +21,7 @@ export default function TabContent() {
                         <TabsTrigger value="pre-wisuda" className="text-xs md:text-base">Pre-Wisuda</TabsTrigger>
                     </TabsList>
                     <TabsContent value="pre-proposal">
-                            <PreProposal />
+                        <PreProposal />
                     </TabsContent>
                     <TabsContent value="pre-sidang">
                         <PreSidang />
@@ -31,8 +31,6 @@ export default function TabContent() {
                     </TabsContent>
                 </Tabs>
             </div>
-
-
         </div>
     )
 }
